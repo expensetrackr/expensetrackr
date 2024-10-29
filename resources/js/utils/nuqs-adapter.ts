6ad883eabf42fc7ab3499d01018bdcf55efdf8bd
danestves <danestves@users.noreply.github.com>
2024-10-29 11:13:24 -0400

    feat: enhance the nuqs adapter

--- resources/js/utils/nuqs-adapter.ts
+++ resources/js/utils/nuqs-adapter.ts
@@ -1,6 +1,20 @@
 import { router } from "@inertiajs/react";
+import mitt from "mitt";
 import { renderQueryString, type unstable_AdapterOptions, unstable_createAdapterProvider } from "nuqs/adapters/custom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const emitter = mitt<{ update: URLSearchParams }>();
+
+function updateUrl(search: URLSearchParams, options: unstable_AdapterOptions) {
+	const url = new URL(window.location.href);
+	url.search = renderQueryString(search);
+	router.visit(url, {
+		replace: options.history === "replace",
+		preserveScroll: !options.scroll,
+		preserveState: options.shallow,
+	});
+	emitter.emit("update", search);
+}
 
 function useNuqsInertiaAdapter() {
 	const [searchParams, setSearchParams] = useState(() => {
@@ -10,15 +24,19 @@ function useNuqsInertiaAdapter() {
 		return new URLSearchParams(location.search);
 	});
 
-	const updateUrl = (search: URLSearchParams, options: unstable_AdapterOptions) => {
-		const url = new URL(window.location.href);
-		url.search = renderQueryString(search);
-
-		router.visit(url, {
-			replace: options.history === "replace",
-			preserveScroll: !options.scroll,
-		});
-	};
+	useEffect(() => {
+		// Popstate event is only fired when the user navigates
+		// via the browser's back/forward buttons.
+		const onPopState = () => {
+			setSearchParams(new URLSearchParams(location.search));
+		};
+		emitter.on("update", setSearchParams);
+		window.addEventListener("popstate", onPopState);
+		return () => {
+			emitter.off("update", setSearchParams);
+			window.removeEventListener("popstate", onPopState);
+		};
+	}, []);
 
 	return {
 		searchParams,
