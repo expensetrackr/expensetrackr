ffd4b7f3cf8095fdc3f635e123c8b31808853acb
danestves <danestves@users.noreply.github.com>
2024-10-28 20:39:04 -0400

    feat: working cancel invitation button

--- resources/js/pages/workspaces/partials/workspace-member-invitations.tsx
+++ resources/js/pages/workspaces/partials/workspace-member-invitations.tsx
@@ -1,6 +1,11 @@
+import DeleteBinIcon from "virtual:icons/ri/delete-bin-line";
+import { router } from "@inertiajs/react";
+import { useState } from "react";
+
 import { ActionSection } from "#/components/action-section";
-import { Button } from "#/components/button.tsx";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "#/components/table.tsx";
+import { Button } from "#/components/button";
+import { Dialog, DialogActions, DialogDescription, DialogHeader, DialogIcon, DialogTitle } from "#/components/dialog";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "#/components/table";
 import type { User, Workspace, WorkspaceInvitation, WorkspacePermissions } from "#/types";
 
 interface UserMembership extends User {
@@ -22,7 +27,7 @@ export function WorkspaceMemberInvitations({ workspace, permissions }: Workspace
 	return (
 		<ActionSection
 			title="Pending workspace invitations"
-			description="These people have been invited to your team and have been sent an invitation email. They may join the team by accepting the email invitation."
+			description="These people have been invited to your workspace and have been sent an invitation email. They may join the workspace by accepting the email invitation."
 		>
 			<Table bleed>
 				<TableHead>
@@ -42,9 +47,7 @@ export function WorkspaceMemberInvitations({ workspace, permissions }: Workspace
 								<TableCell>{invitation.email}</TableCell>
 								{permissions.canRemoveWorkspaceMembers ? (
 									<TableCell>
-										<Button $color="error" $variant="stroke" $size="sm">
-											Cancel invitation
-										</Button>
+										<CancelInvitation invitation={invitation} />
 									</TableCell>
 								) : null}
 							</TableRow>
@@ -61,5 +64,63 @@ export function WorkspaceMemberInvitations({ workspace, permissions }: Workspace
 				</TableBody>
 			</Table>
 		</ActionSection>
+	);
+}
+
+function CancelInvitation({ invitation }: { invitation: WorkspaceInvitation }) {
+	const [isOpen, setOpen] = useState(false);
+	const [isCancelling, setCancelling] = useState(false);
+
+	function cancelWorkspaceInvitation(invitation: WorkspaceInvitation) {
+		setCancelling(true);
+		router.delete(route("workspace-invitations.destroy", [invitation]), {
+			preserveScroll: true,
+		});
+
+		setOpen(false);
+		setCancelling(false);
+	}
+
+	return (
+		<>
+			<Button $color="error" $variant="stroke" $size="sm" onClick={() => setOpen(true)}>
+				Cancel invitation
+			</Button>
+
+			<Dialog open={isOpen} onClose={setOpen}>
+				<DialogHeader>
+					<DialogIcon>
+						<DeleteBinIcon className="size-6 text-[var(--icon-sub-600)]" />
+					</DialogIcon>
+
+					<div className="flex flex-1 flex-col gap-1">
+						<DialogTitle>Cancel invitation</DialogTitle>
+						<DialogDescription>Are you sure you want to cancel this invitation?</DialogDescription>
+					</div>
+				</DialogHeader>
+
+				<DialogActions>
+					<Button
+						$color="neutral"
+						$variant="stroke"
+						$size="sm"
+						className="w-full"
+						disabled={isCancelling}
+						onClick={() => setOpen(false)}
+					>
+						Cancel
+					</Button>
+					<Button
+						$color="error"
+						$size="sm"
+						className="w-full"
+						disabled={isCancelling}
+						onClick={() => cancelWorkspaceInvitation(invitation)}
+					>
+						{isCancelling ? "Cancelling..." : "Cancel invitation"}
+					</Button>
+				</DialogActions>
+			</Dialog>
+		</>
 	);
 }
