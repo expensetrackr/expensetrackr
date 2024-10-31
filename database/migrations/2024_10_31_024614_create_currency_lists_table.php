b3979e1f8ef006589c82d89aa435b8553d9fa551
danestves <danestves@users.noreply.github.com>
2024-10-30 23:41:24 -0400

    fix: dont need to call the service to see if is enabled or not

--- database/migrations/2024_10_31_024614_create_currency_lists_table.php
+++ database/migrations/2024_10_31_024614_create_currency_lists_table.php
@@ -2,19 +2,14 @@
 
 declare(strict_types=1);
 
-use App\Services\CurrencyService;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    public function up(CurrencyService $currency): void
+    public function up(): void
     {
-        if (! $currency->isEnabled()) {
-            return;
-        }
-
         Schema::create('currency_lists', function (Blueprint $table) {
             $table->id();
             $table->string('code');
