69004673f794cdebb6bb59569977e59c81ff1b33
danestves <danestves@users.noreply.github.com>
2024-11-13 01:47:39 -0400

    feat: change balance of accounts to decimals

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -52,7 +52,7 @@ final class AppServiceProvider extends ServiceProvider
      */
     private function configurePasswordValidation(): void
     {
-        Password::defaults(fn() => app()->isProduction() ? Password::min(8)->uncompromised() : null);
+        Password::defaults(fn () => app()->isProduction() ? Password::min(8)->uncompromised() : null);
     }
 
     /**
