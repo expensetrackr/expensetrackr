9a69fb21617298ba16e8e81a73e6be20f5e28754
danestves <danestves@users.noreply.github.com>
2024-11-15 02:07:13 -0400

    chore: delete test toast lol

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::middleware([
     'verified',
 ])->group(function () {
     Route::get('/dashboard', function () {
-        return Inertia::render('dashboard')->with('toast', ['type' => 'success', 'message' => 'Hello, world!']);
+        return Inertia::render('dashboard');
     })->name('dashboard');
 
     Route::get('/accounts', [AccountController::class, 'index'])->name('accounts.index');
