d16404d4f684b9d85c9b97c9c8d29987cc3c8ff7
danestves <danestves@users.noreply.github.com>
2024-12-20 17:33:56 -0400

    chore: update dependencies

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -5,16 +5,10 @@ import "../css/app.css";
 import { createInertiaApp } from "@inertiajs/react";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { createRoot, hydrateRoot } from "react-dom/client";
-import { scan } from "react-scan";
 
 import { NuqsAdapter } from "#/utils/nuqs-adapter.ts";
 
 const appName = import.meta.env.VITE_APP_NAME || "ExpenseTrackr";
-
-scan({
-    enabled: import.meta.env.DEV && localStorage.getItem("react-scan") === "true",
-    log: import.meta.env.DEV && localStorage.getItem("react-scan") === "true",
-});
 
 void createInertiaApp({
     title: (title) => `${title} - ${appName}`,
