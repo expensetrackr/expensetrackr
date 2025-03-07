8c73c812ccb8c587e327c5f30cef2c781456821b
danestves <danestves@users.noreply.github.com>
2025-03-07 01:23:59 -0400

    chore: update dependencies

--- resources/js/utils/forms/create-account.form.ts
+++
@@ -1,9 +0,0 @@
-import { createFormHook, createFormHookContexts } from "@tanstack/react-form";
-
-const { fieldContext, formContext } = createFormHookContexts();
-export const { useAppForm, withForm } = createFormHook({
-    fieldComponents: {},
-    formComponents: {},
-    fieldContext,
-    formContext,
-});
