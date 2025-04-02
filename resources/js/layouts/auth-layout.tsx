317fe4044100ca9efd17e423b3ccc7e0d266f82d
danestves <danestves@users.noreply.github.com>
2025-04-02 15:24:55 -0400

    feat: replace img tag with Image component in AuthLayout

--- resources/js/layouts/auth-layout.tsx
+++ resources/js/layouts/auth-layout.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { toast } from "sonner";
 
+import { Image } from "#/components/image.tsx";
 import { type PageProps } from "#/types/globals.js";
 import { AuthFooter } from "./partials/auth-footer.tsx";
 import { AuthHeader } from "./partials/auth-header.tsx";
@@ -25,12 +26,12 @@ export function AuthLayout({
             <AuthHeader />
 
             <div className="relative isolate flex w-full flex-1 flex-col items-center justify-center">
-                <img
+                <Image
                     alt=""
                     className="pointer-events-none absolute top-1/2 left-1/2 -z-10 w-full max-w-[1140px] -translate-x-1/2 -translate-y-1/2 object-contain"
-                    height="318"
+                    height={318}
                     src="/img/auth-pattern.svg"
-                    width="824"
+                    width={824}
                 />
                 {children}
             </div>
