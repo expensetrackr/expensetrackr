cf7dc3156f6d635e8745ea563e1785f2d297e452
danestves <danestves@users.noreply.github.com>
2025-04-11 02:50:56 -0400

    refactor: comment out 'transfer' option in TransactionType picklist for future feature implementation

--- resources/js/schemas/enums.ts
+++ resources/js/schemas/enums.ts
@@ -2,4 +2,8 @@ import * as v from "valibot";
 
 export const TransactionRecurringInterval = v.picklist(["daily", "weekly", "monthly", "quarterly", "yearly"]);
 
-export const TransactionType = v.picklist(["expense", "income", "transfer"]);
+export const TransactionType = v.picklist([
+    "expense",
+    "income",
+    // "transfer", TODO: enable this once we have a transfer feature
+]);
