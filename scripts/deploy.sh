e156e956105e63b500a28ad334700007e254162e
danestves <danestves@users.noreply.github.com>
2025-04-17 01:12:09 -0400

    chore: add Sentry CLI installation and release management to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -24,3 +24,17 @@ php artisan queue:restart
 php artisan make:sitemap
 
 php artisan up
+
+# Install the cli
+curl -sL https://sentry.io/get-cli/ | bash
+
+# Setup configuration values
+SENTRY_AUTH_TOKEN="${SENTRY_AUTH_TOKEN:?'SENTRY_AUTH_TOKEN environment variable is required'}"
+SENTRY_ORG="${SENTRY_ORG:?'SENTRY_ORG environment variable is required'}"
+SENTRY_PROJECT="${SENTRY_PROJECT:?'SENTRY_PROJECT environment variable is required'}"
+VERSION=`sentry-cli releases propose-version`
+
+# Workflow to create releases
+sentry-cli releases new "$VERSION"
+sentry-cli releases set-commits "$VERSION" --auto
+sentry-cli releases finalize "$VERSION"
