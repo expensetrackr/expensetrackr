fc43eb9200b1e84100f1bcc9039245258afae943
danestves <danestves@users.noreply.github.com>
2025-04-18 15:47:30 -0400

    refactor: remove unused password validation configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Validation\Rules\Password;
 
 final class AppServiceProvider extends ServiceProvider
 {
@@ -19,14 +18,6 @@ final class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        $this->configurePasswordValidation();
-    }
-
-    /**
-     * Configure the password validation rules.
-     */
-    private function configurePasswordValidation(): void
-    {
-        Password::defaults(fn () => app()->isProduction() ? Password::min(8)->uncompromised() : null);
+        //
     }
 }
