e0dabc00565c68f305b9d8b79f2c4601d222e03b
danestves <danestves@users.noreply.github.com>
2025-04-22 14:26:05 -0400

    refactor: remove ThrottleRequests middleware from API configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,7 +10,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
-use Illuminate\Routing\Middleware\ThrottleRequests;
 use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 use Sentry\Laravel\Integration;
 
@@ -31,7 +30,6 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
         $middleware->api(append: [
             EnsureFrontendRequestsAreStateful::class,
-            ThrottleRequests::class.':api',
             HandleWorkspacesPermissionMiddleware::class,
             AddWorkspaceToRequest::class,
         ]);
