6c054dc18191d0184090935e49af916474fa3928
danestves <danestves@users.noreply.github.com>
2025-05-01 17:25:12 -0400

    refactor: remove react-twc utility and update label component

--- resources/js/components/ui/label.tsx
+++ resources/js/components/ui/label.tsx
@@ -1,17 +1,44 @@
 import { Label as LabelPrimitives } from "radix-ui";
 
-import { twc } from "#/utils/twc.ts";
+import { cn } from "#/utils/cn.ts";
 
-export const Root = twc(LabelPrimitives.Root).attrs<
-    React.ComponentPropsWithoutRef<typeof LabelPrimitives.Root> & {
-        disabled?: boolean;
-    }
->((props) => ({
-    "aria-disabled": props.disabled,
-}))`group cursor-pointer text-label-sm flex items-center gap-px aria-disabled:text-(--text-disabled-300)`;
+type LabelRootProps = React.ComponentPropsWithoutRef<typeof LabelPrimitives.Root> & {
+    disabled?: boolean;
+};
 
-export const Asterisk = twc.span.attrs({
-    children: "*",
-})`text-primary group-aria-disabled:text-(--text-disabled-300)`;
+function LabelRoot({ className, ...props }: LabelRootProps) {
+    return (
+        <LabelPrimitives.Root
+            className={cn(
+                "group flex cursor-pointer items-center gap-px text-label-sm aria-disabled:text-(--text-disabled-300)",
+                {
+                    "aria-disabled": props.disabled,
+                },
+                className,
+            )}
+            {...props}
+        />
+    );
+}
 
-export const Sub = twc.span`text-paragraph-sm text-(--text-sub-600) group-aria-disabled:text-(--text-disabled-300)`;
+function LabelAsterisk({ className, ...props }: React.ComponentPropsWithRef<"span">) {
+    return (
+        <span className={cn("text-primary group-aria-disabled:text-(--text-disabled-300)", className)} {...props}>
+            *
+        </span>
+    );
+}
+
+function LabelSub({ className, ...props }: React.ComponentPropsWithRef<"span">) {
+    return (
+        <span
+            className={cn(
+                "text-paragraph-sm text-(--text-sub-600) group-aria-disabled:text-(--text-disabled-300)",
+                className,
+            )}
+            {...props}
+        />
+    );
+}
+
+export { LabelRoot as Root, LabelAsterisk as Asterisk, LabelSub as Sub };
