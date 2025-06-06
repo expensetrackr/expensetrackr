fd75885b456021aa22db4b211951ab84b9ec6a52
danestves <danestves@users.noreply.github.com>
2025-06-06 15:20:22 -0400

    fix: change import method for JSDOM in sanitize utility

--- resources/js/utils/sanitize.ts
+++ resources/js/utils/sanitize.ts
@@ -3,7 +3,7 @@ import DOMPurify from "dompurify";
 let purify: ReturnType<typeof DOMPurify>;
 
 if (typeof window === "undefined") {
-    const { JSDOM } = await import("jsdom");
+    const { JSDOM } = require("jsdom");
     purify = DOMPurify(new JSDOM("").window);
 } else {
     purify = DOMPurify(window);
