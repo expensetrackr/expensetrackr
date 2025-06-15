e7ff2c37ffa027a19328bba3ea3b9e609d61e0d6
danestves <danestves@users.noreply.github.com>
2025-06-15 17:09:00 -0400

    chore: add spatie health checks for CPU load and security advisories in HealthServiceProvider

--- app/Providers/HealthServiceProvider.php
+++ app/Providers/HealthServiceProvider.php
@@ -5,12 +5,18 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Spatie\CpuLoadHealthCheck\CpuLoadCheck;
 use Spatie\Health\Checks\Checks\CacheCheck;
 use Spatie\Health\Checks\Checks\DatabaseCheck;
 use Spatie\Health\Checks\Checks\EnvironmentCheck;
+use Spatie\Health\Checks\Checks\HorizonCheck;
+use Spatie\Health\Checks\Checks\OptimizedAppCheck;
+use Spatie\Health\Checks\Checks\QueueCheck;
+use Spatie\Health\Checks\Checks\RedisCheck;
 use Spatie\Health\Checks\Checks\ScheduleCheck;
 use Spatie\Health\Checks\Checks\UsedDiskSpaceCheck;
 use Spatie\Health\Facades\Health;
+use Spatie\SecurityAdvisoriesHealthCheck\SecurityAdvisoriesCheck;
 
 final class HealthServiceProvider extends ServiceProvider
 {
@@ -29,9 +35,17 @@ final class HealthServiceProvider extends ServiceProvider
     {
         Health::checks([
             CacheCheck::new(),
+            OptimizedAppCheck::new(),
+            CpuLoadCheck::new()
+                ->failWhenLoadIsHigherInTheLast5Minutes(2.0)
+                ->failWhenLoadIsHigherInTheLast15Minutes(1.5),
             DatabaseCheck::new(),
             EnvironmentCheck::new(),
+            HorizonCheck::new(),
+            QueueCheck::new(),
+            RedisCheck::new(),
             ScheduleCheck::new(),
+            SecurityAdvisoriesCheck::new(),
             UsedDiskSpaceCheck::new(),
         ]);
     }
