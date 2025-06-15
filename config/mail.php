25d48f309d9f3c5e8d5b761bb1948de4142e62ed
Daniel Esteves <estevesd8@gmail.com>
2025-06-15 16:12:31 -0400

    chore: add Laravel Horizon package and configure service provider (#47)
    Co-authored-by: danestves <danestves@users.noreply.github.com>

--- config/mail.php
+++ config/mail.php
@@ -109,6 +109,7 @@ return [
     | used globally for all emails that are sent by your application.
     |
     */
+    'to' => env('MAIL_TO_ADDRESS', 'hello@example.com'),
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
