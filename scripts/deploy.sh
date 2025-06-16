347918d657a137b4af4d0d86ffaabe2251fa6472
danestves <danestves@users.noreply.github.com>
2025-06-15 20:27:13 -0400

    chore: add caching commands to deploy script for config, view, event, and route

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,12 +5,16 @@ php artisan down
 php artisan cache:clear
 
 php artisan config:clear
+php artisan config:cache
 
 php artisan view:clear
+php artisan view:cache
 
 php artisan event:clear
+php artisan event:cache
 
 php artisan route:clear
+php artisan route:cache
 
 php artisan filament:clear-cached-components
 php artisan filament:cache-components
