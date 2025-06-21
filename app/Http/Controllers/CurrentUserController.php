3c6a8b21ee7a0d425aa61d96982fef7e319dc7fd
danestves <danestves@users.noreply.github.com>
2025-06-20 21:07:12 -0400

    feat: implement password confirmation logic for user account deletion

--- app/Http/Controllers/CurrentUserController.php
+++ app/Http/Controllers/CurrentUserController.php
@@ -11,6 +11,7 @@ use App\Models\User;
 use Illuminate\Contracts\Auth\StatefulGuard;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Date;
 use Inertia\Inertia;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -33,7 +34,21 @@ final class CurrentUserController extends Controller
     {
         $user = type($request->user())->as(User::class);
 
-        // TODO: Implement password confirmation
+        $lastConfirmation = $request->session()->get(
+            'auth.password_confirmed_at', 0
+        );
+
+        $lastConfirmed = Date::now()->unix() - $lastConfirmation;
+
+        $confirmed = $lastConfirmed < $request->input(
+            'seconds', config('auth.password_timeout', 900)
+        );
+
+        if (! $confirmed) {
+            return back(303, [
+                'seconds' => config('auth.password_timeout', 900) - $lastConfirmed,
+            ]);
+        }
 
         $action->handle($user);
 
