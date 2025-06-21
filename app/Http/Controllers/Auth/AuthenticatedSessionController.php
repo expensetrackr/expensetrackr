28ac2a253a6c42d5edd3c7e00504e6627d78e65c
danestves <danestves@users.noreply.github.com>
2025-06-20 21:08:16 -0400

    refactor: streamline user authentication process by removing password check and utilizing request authentication

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -6,11 +6,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
-use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -33,10 +31,14 @@ final class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): RedirectResponse
     {
-        $user = User::where('email', $request->email)->first();
+        $request->authenticate();
 
-        // If this user exists, password is correct, and 2FA is enabled, we want to redirect to the 2FA challenge
-        if ($user && $user->two_factor_confirmed_at && Hash::check($request->password, $user->password)) {
+        $user = $request->user();
+
+        // If this user exists, and 2FA is enabled, we want to redirect to the 2FA challenge
+        if ($user && $user->two_factor_confirmed_at) {
+            Auth::logout();
+
             // Store the user ID and remember preference in the session
             $request->session()->put([
                 'login.id' => $user->getKey(),
@@ -45,9 +47,6 @@ final class AuthenticatedSessionController extends Controller
 
             return redirect()->route('two-factor.challenge');
         }
-
-        // Otherwise, proceed with normal authentication
-        $request->authenticate();
 
         $request->session()->regenerate();
 
